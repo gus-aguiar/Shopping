6b538c3314b80687903205689e6de82439c07455
gus-aguiar <gustavopma90@gmail.com>
2022-11-16 20:09:14 -0300

    fiz a 5, fazendo a 6

--- src/main.js
+++ src/main.js
@@ -17,14 +17,25 @@ const apagaLoading = () => {
   const textoLoading = document.querySelector('.loading');
   textoLoading.remove();
 };
+const createErrorText = () => {
+  const productos = document.querySelector('.products');
+  const textoErro = document.createElement('h1');
+  textoErro.className = 'error';
+  textoErro.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  productos.appendChild(textoErro);
+};
 
 const createProducts = async (param) => {
   criaLoading();
-  const computador = await fetchProductsList(param);
-  computador.forEach((element) => {
-    products.appendChild(createProductElement(element));
-  });
-  apagaLoading();
+  try {
+    const computador = await fetchProductsList(param);
+    computador.forEach((element) => {
+      products.appendChild(createProductElement(element));
+    });
+    apagaLoading();
+  } catch (error) {
+    return createErrorText();
+  }
 };
 
 await createProducts('computador');
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,11 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProductsList é uma função', () => {
+    expect(typeof fetchProduct).toBe('function')
+  });
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProduct("MLB1405519561");
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
   });
 });
