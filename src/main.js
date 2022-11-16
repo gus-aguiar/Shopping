2b9009ee100db9cda8f95fa93c2852bfe9b0d986
gus-aguiar <gustavopma90@gmail.com>
2022-11-16 17:15:58 -0300

    fiz a 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const products = document.querySelector('.products');
+const computador = await fetchProductsList('computador');
+computador.forEach((element) => {
+  const data = createProductElement(element);
+  products.appendChild(data);
+});
