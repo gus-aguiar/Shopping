b49b28b68be66c4f9d1f228838f43e1398a5e090
gus-aguiar <gustavopma90@gmail.com>
2022-11-16 17:51:29 -0300

    fiz alguma coisa da 3

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const carregando = document.querySelector('.carregando');
 const products = document.querySelector('.products');
 const computador = await fetchProductsList('computador');
+if (computador) { carregando.style.display = 'none'; }
+if (!computador) { carregando.style.display = 'flex'; }
 computador.forEach((element) => {
   const data = createProductElement(element);
   products.appendChild(data);
