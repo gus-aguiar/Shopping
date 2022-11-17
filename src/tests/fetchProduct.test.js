3cc06c4941c926ba80c26b7213a1006044903ce6
gus-aguiar <gustavopma90@gmail.com>
2022-11-17 10:52:00 -0300

    first commit

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -19,12 +19,8 @@ describe('Teste a função fetchProduct', () => {
     const data = await fetchProduct("MLB1405519561")
     expect(data).toEqual(product)
   });
-  it("Teste se, ao chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: 'ID não informado'.", async () => {
-    try{
-      await fetchProduct()
-    } catch(error){
-      expect(error.message).toBe('ID não informado')
-    }
+  it("Teste se, ao chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: 'ID não informado'.", async () => {  
+      expect(await fetchProduct()).toThrow(new Error('yuck, octopus flavor'))
 });
 
 });
