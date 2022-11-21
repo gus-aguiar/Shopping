d6c6b686c54e03fafdf3767d7ab0bfb2a639e1b5
gus-aguiar <gustavopma90@gmail.com>
2022-11-21 17:14:50 -0300

    fiz a onze, final commit

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,24 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (endpoint) => {
+  const URL1 = `https://cep.awesomeapi.com.br/json/${endpoint}`;
+  const URL2 = `https://brasilapi.com.br/api/cep/v2/${endpoint}`;
+  const fetch1 = await fetch(URL1);
+  const fetch2 = await fetch(URL2);
+  const response = await Promise.any([fetch1, fetch2]);
+  const data = await response.json();
+  console.table(data);
+  return data;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cep = document.querySelector('.cep-input').value;
+  const display = document.querySelector('.cart__address');
+  getAddress(cep)
+    .then((data) => {
+      const Rua = data.address;
+      const Bairro = data.district;
+      const Cidade = data.city;
+      const Estado = data.state;
+      display.innerText = `${Rua} - ${Bairro} - ${Cidade} - ${Estado}`;
+    })
+    .catch(() => { display.innerText = 'CEP não encontrado'; });
 };
